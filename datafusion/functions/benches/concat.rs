0acf057633f400e93e4336d74c726cfd614ad83f
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-09-13 13:39:08 +0200

    bench changes

--- datafusion/functions/benches/concat.rs
+++ datafusion/functions/benches/concat.rs
@@ -18,28 +18,39 @@
 use arrow::array::ArrayRef;
 use arrow::util::bench_util::create_string_array_with_len;
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use datafusion_common::ScalarValue;
 use datafusion_expr::ColumnarValue;
-use datafusion_functions::string::concat;
 use std::sync::Arc;
 
 fn create_args(size: usize, str_len: usize) -> Vec<ColumnarValue> {
-    let array = Arc::new(create_string_array_with_len::<i32>(size, 0.2, str_len));
-    let scalar = ScalarValue::Utf8(Some(", ".to_string()));
+    let array1 = Arc::new(create_string_array_with_len::<i32>(size, 0.2, str_len));
+    let array2 = Arc::new(create_string_array_with_len::<i32>(size, 0.2, str_len));
     vec![
-        ColumnarValue::Array(Arc::clone(&array) as ArrayRef),
-        ColumnarValue::Scalar(scalar),
-        ColumnarValue::Array(array),
+        ColumnarValue::Array(Arc::clone(&array1) as ArrayRef),
+        ColumnarValue::Array(Arc::clone(&array2) as ArrayRef),
     ]
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
+    use datafusion_expr::ScalarUDFImpl;
+    let concat = datafusion_functions::string::concat::ConcatFunc::default();
+    let my_concat = datafusion_functions::string::my_concat::MyConcatFunc::default();
+
     for size in [1024, 4096, 8192] {
         let args = create_args(size, 32);
-        let mut group = c.benchmark_group("concat function");
-        group.bench_function(BenchmarkId::new("concat", size), |b| {
-            b.iter(|| criterion::black_box(concat().invoke(&args).unwrap()))
-        });
+        let mut group = c.benchmark_group("benchmark_group");
+
+        group.bench_with_input(
+            BenchmarkId::new("concat", size),
+            &args,
+            |b, args| b.iter(|| criterion::black_box(/**/ my_concat.invoke(args).unwrap())),
+        );
+
+        // group.bench_with_input(
+        //     BenchmarkId::new("my_concat", size),
+        //     &args,
+        //     |b, args| b.iter(|| criterion::black_box(my_concat.invoke(args).unwrap())),
+        // );
+
         group.finish();
     }
 }
