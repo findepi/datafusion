3fb415133ca562c0dab248885bdaace482a575ee
Piotr Findeisen <piotr.findeisen@gmail.com>
2025-02-14 22:14:49 +0100

    fixup! string support

--- datafusion/excalibur/lib/tests/str_.rs
+++ datafusion/excalibur/lib/tests/str_.rs
@@ -15,7 +15,6 @@
 // specific language governing permissions and limitations
 // under the License.
 
-
 // #[excalibur_function]
 // fn concat(a: &str, b: &str, output: &mut impl std::fmt::Write) -> () {
 // }
