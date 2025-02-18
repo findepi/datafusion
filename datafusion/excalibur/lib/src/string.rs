657be21d0a15bb3837c200d1252cf7afb8d230e2
Piotr Findeisen <piotr.findeisen@gmail.com>
2025-02-18 08:20:29 +0100

    Revert "TRY partial string as &str"
    This reverts commit e58d225883fbd234c0a7a80a8cf6eb4272ff8926.

--- datafusion/excalibur/lib/src/string.rs
+++ datafusion/excalibur/lib/src/string.rs
@@ -55,10 +55,10 @@ impl ExArgType for ImplAsRefStr {
             },
 
             ColumnarValue::Scalar(ScalarValue::Utf8(value)) => {
-                consumer.consume(&ScalarString(value))
+                consumer.consume(ScalarString(value))
             }
             ColumnarValue::Scalar(ScalarValue::Utf8View(value)) => {
-                consumer.consume(&ScalarString(value))
+                consumer.consume(ScalarString(value))
             }
 
             ColumnarValue::Scalar(scalar) => {
@@ -70,40 +70,40 @@ impl ExArgType for ImplAsRefStr {
 
 // TODO implement this in terms of GenericByteArray
 
-impl<'a> ExArrayReader for &'a StringArray {
-    type ValueType = &'a str;
+impl ExArrayReader for &StringArray {
+    type ValueType = ImplAsRefStr;
 
     fn is_valid(&self, position: usize) -> bool {
         Array::is_valid(&self, position)
     }
 
     fn get(&self, position: usize) -> Self::ValueType {
-        (self.value(position).into())
+        ImplAsRefStr(self.value(position).into())
     }
 }
 
-impl<'a> ExArrayReader for &'a StringViewArray {
-    type ValueType = &'a str;
+impl ExArrayReader for &StringViewArray {
+    type ValueType = ImplAsRefStr;
 
     fn is_valid(&self, position: usize) -> bool {
         Array::is_valid(&self, position)
     }
 
     fn get(&self, position: usize) -> Self::ValueType {
-        (self.value(position).into())
+        ImplAsRefStr(self.value(position).into())
     }
 }
 
 struct ScalarString(Option<String>);
 
-impl<'a> ExArrayReader for &'a ScalarString {
-    type ValueType = &'a str;
+impl ExArrayReader for ScalarString {
+    type ValueType = ImplAsRefStr;
 
     fn is_valid(&self, _position: usize) -> bool {
         self.0.is_some()
     }
 
     fn get(&self, _position: usize) -> Self::ValueType {
-        (self.0.as_deref().unwrap().into())
+        ImplAsRefStr(self.0.as_deref().unwrap().into())
     }
 }
