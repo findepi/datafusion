a7a58062f2ea56136670e6fb0ed7c7f37685aba8
Piotr Findeisen <piotr.findeisen@gmail.com>
2025-02-21 17:16:33 +0100

    fixup! decimal stub

--- datafusion/excalibur/lib/src/decimal.rs
+++ datafusion/excalibur/lib/src/decimal.rs
@@ -18,26 +18,64 @@
 use crate::__private::ExInstantiable;
 use crate::arg_type::ExArgType;
 use crate::reader::{ExArrayReader, ExArrayReaderConsumer};
-use arrow::array::{ArrowPrimitiveType, Decimal128Array, PrimitiveArray};
+use arrow::array::{Array, ArrowPrimitiveType, Decimal128Array, PrimitiveArray};
 use arrow::datatypes::{Decimal128Type, DecimalType};
 use datafusion_common::cast::as_decimal128_array;
-use datafusion_common::internal_err;
 use datafusion_common::types::NativeType;
+use datafusion_common::Result;
+use datafusion_common::{internal_err, ScalarValue};
 use datafusion_expr_common::columnar_value::ColumnarValue;
 
 // We could use `arrow::datatypes::DecimalType` here, but that would unnecessarily
 // tie the usage to Arrow.
-pub struct Decimal<Native> {
+pub struct Decimal<Native: DecimalNativeType> {
     pub unscaled_value: Native,
     pub precision: u8,
     pub scale: i8,
 }
 
-impl ExInstantiable for Decimal<i128> {
+// seal and adapt
+trait DecimalNativeType: Sized {
+    type ArrowDecimalType: ArrowPrimitiveType + DecimalType;
+
+    fn cast_decimal_array(
+        array: &dyn Array,
+    ) -> Result<&PrimitiveArray<Self::ArrowDecimalType>>;
+
+    fn cast_scalar(
+        scalar: ScalarValue,
+    ) -> std::result::Result<(Option<Self>, u8, i8), ScalarValue>;
+}
+impl DecimalNativeType for i128 {
+    type ArrowDecimalType = Decimal128Type;
+
+    fn cast_decimal_array(
+        array: &dyn Array,
+    ) -> Result<&PrimitiveArray<Self::ArrowDecimalType>> {
+        as_decimal128_array(array)
+    }
+
+    fn cast_scalar(
+        scalar: ScalarValue,
+    ) -> std::result::Result<(Option<Self>, u8, i8), ScalarValue> {
+        if let ScalarValue::Decimal128(value, precision, scale) = scalar {
+            Ok((value, precision, scale))
+        } else {
+            Err(scalar)
+        }
+    }
+}
+
+impl<Native: DecimalNativeType> ExInstantiable for Decimal<Native> {
     type StackType<'a> = Self;
 }
 
-impl ExArgType for Decimal<i128> {
+impl<Native: DecimalNativeType> ExArgType for Decimal<Native>
+where
+    for<'a> DecimalReader<&'a PrimitiveArray<Native::ArrowDecimalType>>:
+        ExArrayReader<'a, ValueType = Decimal<Native>>,
+    for<'a> DecimalReader<Option<Native>>: ExArrayReader<'a, ValueType = Decimal<Native>>,
+{
     fn logical_type() -> NativeType {
         // This should be generic, a pattern over NativeType, so that
         // we allow accepting e.g. decimal(p, 0) or any decimal(p, s) without any
@@ -58,33 +96,32 @@ impl ExArgType for Decimal<i128> {
     fn decode(
         arg: ColumnarValue,
         consumer: impl for<'a> ExArrayReaderConsumer<ValueType<'a> = Self::StackType<'a>>,
-    ) -> datafusion_common::Result<()> {
+    ) -> Result<()> {
         use datafusion_common::ScalarValue;
         use datafusion_expr::ColumnarValue::*;
         match arg {
             Array(array) => {
-                let decimal_array = as_decimal128_array(&array)?;
+                let decimal_array = Native::cast_decimal_array(&array)?;
                 consumer.consume(DecimalReader {
                     delegate: decimal_array,
                     precision: decimal_array.precision(),
                     scale: decimal_array.scale(),
                 })
             }
-            Scalar(scalar) => {
-                if let ScalarValue::Decimal128(value, precision, scale) = scalar {
-                    consumer.consume(DecimalReader {
-                        delegate: value,
-                        precision,
-                        scale,
-                    })
-                } else {
+            Scalar(scalar) => match Native::cast_scalar(scalar) {
+                Ok((value, precision, scale)) => consumer.consume(DecimalReader {
+                    delegate: value,
+                    precision,
+                    scale,
+                }),
+                Err(scalar) => {
                     internal_err!(
                         "Expected {} scalar, got: {:?}",
                         stringify!($native_type),
                         scalar
                     )
                 }
-            }
+            },
         }
     }
 }
@@ -95,11 +132,12 @@ struct DecimalReader<Delegate> {
     scale: i8,
 }
 
-impl<'a, Delegate> ExArrayReader<'_> for DecimalReader<Delegate>
+impl<'a, Delegate> ExArrayReader<'a> for DecimalReader<Delegate>
 where
-    Delegate: ExArrayReader<'a, ValueType = i128>,
+    Delegate: ExArrayReader<'a>,
+    Delegate::ValueType: DecimalNativeType,
 {
-    type ValueType = Decimal<i128>;
+    type ValueType = Decimal<Delegate::ValueType>;
 
     fn assert_number_rows(&self, number_rows: usize) {
         self.delegate.assert_number_rows(number_rows);
